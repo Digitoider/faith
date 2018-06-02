58eca8d973d05eef48eb638d173ea46b8f6b112a
Alexander <a.brjakin@avia-centr.ru>
2018-06-02 03:06:57 +0300

    Generate ScheduleController.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # get 'profile', to
   resources :profile, only: %i[index]
   resources :analysis, only: %i[new create]
+  resources :schedule, only: %i[new create]
   # get 'add_analysis', to: 'analysis#new'
   # post 'add_analysis', to: 'doc'
 
