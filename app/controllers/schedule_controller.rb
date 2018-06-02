383bb548a7c6c03a446e36c13ab08f39125251e1
Alexander <a.brjakin@avia-centr.ru>
2018-06-02 11:20:31 +0300

    Add table with people who require operation (right side).

--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -2,6 +2,11 @@ class ScheduleController < ApplicationController
 
   def new
     @operation_day = OperationDay.new
+    @operations = Operation.where(is_operated: false)
+    @profiles_fio = @operations.map do |op|
+      p = op.analysis.profile
+      [p.surname, p.name, p.middlename].join ' '
+    end
     render
   end
 
